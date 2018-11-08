82506b3a61b9eeed848bbb91192aaf10f35c5613
todokr <shunsuke.tadokoro@bizreach.co.jp>
2018-11-08 23:03:51 +0900

    fix ast

--- src/main/scala/ast/Ast.scala
+++ src/main/scala/ast/Ast.scala
@@ -1,10 +1,14 @@
 package ast
 
-/**
- * Created by todokr on 18/02/11.
- */
 sealed trait Ast
-case class CreateDatabase() extends Ast
-case class CreateTable() extends Ast
-case class Insert() extends Ast
-case class Select() extends Ast
+case class Select(columns: Seq[Column]) extends Ast
+case class From(tables: Seq[Table])
+case class OrderBy(orders: Seq[Order])
+
+case class Table()
+case class Column()
+case class Order(column: Column, ordering: Ordering)
+
+sealed trait Ordering
+case object Asc extends Ordering
+case object Desc extends Ordering
